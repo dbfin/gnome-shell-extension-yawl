de79364769fe1af352083829ffdc6dff0ebfb5e9
vadim <vadim@dbfin.com>
2013-05-20 13:03:18 -0500

    Versioning consistent with extensions.gnome.org.

--- include.mk
+++ include.mk
@@ -1,7 +1,7 @@
 EXTENSION_ID = gnome-shell-extension-yawl
 EXTENSION_ID_SHORT = yawl
 
-extensionversion = 13.05
+extensionversion = 3
 
 extensionbase = @dbfin.com
 uuid = $(EXTENSION_ID_SHORT)$(extensionbase)
