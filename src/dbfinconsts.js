c6aa77799e69f00c569c5f5adc52db362aecd40e
vadim <vadim@dbfin.com>
2013-07-01 18:49:13 -0500

    Remove New Window from standard menu as this is usually provided in Quicklists or Remote menus.

--- src/dbfinconsts.js
+++ src/dbfinconsts.js
@@ -83,8 +83,8 @@ const arrayWindowClickFunctions = [
 ];
 
 const arrayAppMenuItems = [
-    [   _("New window"),            					'openNewWindowThisWorkspace'	],
-    [   '',                         					null							],
+//    [   _("New window"),            					'openNewWindowThisWorkspace'	],
+//    [   '',                         					null							],
     [   _("Quit"),                  					'quitApplication'				]
 ];
 
