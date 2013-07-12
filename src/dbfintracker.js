c911b4ff3f4f94347c7627fa408065571527fe2d
vadim <vadim@dbfin.com>
2013-07-12 16:17:37 -0500

    Check whether a window requires attention after trackerWindow has been created.

--- src/dbfintracker.js
+++ src/dbfintracker.js
@@ -204,6 +204,9 @@ const dbFinTracker = new Lang.Class({
 							windowsIn.push(metaWindow);
                             this.windows.set(metaWindow, trackerWindow = new dbFinTrackerWindow.dbFinTrackerWindow(metaWindow, this, metaApp, this.state));
 							trackerApp.addWindow(metaWindow);
+							if (metaWindow.demans_attention || metaWindow.urgent) {
+								this.addAppWindowAttention(metaApp, metaWindow);
+							}
 						}
                         else {
                             trackerWindow.state = this.state;
