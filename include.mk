d527d26dfd58dfb4df58e488b1e6204aa70c0156
vadim <vadim@dbfin.com>
2013-11-06 19:10:09 -0600

    Version 13.

--- include.mk
+++ include.mk
@@ -1,7 +1,7 @@
 EXTENSION_ID = gnome-shell-extension-yawl
 EXTENSION_ID_SHORT = yawl
 
-extensionversion = 12
+extensionversion = 13
 
 extensionbase = @dbfin.com
 uuid = $(EXTENSION_ID_SHORT)$(extensionbase)
