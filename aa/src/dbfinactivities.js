e22c2fba60a7b705d8f7693d9461c7d25f25e526
vadim <vadim@dbfin.com>
2013-12-17 22:07:37 -0600

    Make submenu animation a bit faster than the GS's default.

--- aa/src/dbfinactivities.js
+++ aa/src/dbfinactivities.js
@@ -94,8 +94,8 @@ const dbFinPopupSubMenuMenuItemAutoClose = new Lang.Class({
                     if (this.actor) {
                         this.actor.set_height(-1);
                     }
-                }, this, 250, 'linear', true);
-                dbFinAnimation.animateToState(this._arrow, { rotation_angle_z: 90 }, null, null, 250, 'linear');
+                }, this, 200, 'linear', true);
+                dbFinAnimation.animateToState(this._arrow, { rotation_angle_z: 90 }, null, null, 200, 'linear');
             }
         }
     },
@@ -108,8 +108,8 @@ const dbFinPopupSubMenuMenuItemAutoClose = new Lang.Class({
                     this.actor.hide();
                     this.actor.set_height(-1);
                 }
-            }, this, 250, 'linear', true);
-            dbFinAnimation.animateToState(this._arrow, { rotation_angle_z: 0 }, null, null, 250, 'linear');
+            }, this, 200, 'linear', true);
+            dbFinAnimation.animateToState(this._arrow, { rotation_angle_z: 0 }, null, null, 200, 'linear');
         }
     },
     _removeTopSubmenuOpenedLast: function () {
